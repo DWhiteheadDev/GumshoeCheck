3b24a2cd7dbd2b84701ef4ddc34a17a12d720331
Dylan Whitehead <dwhiteheaddev@github.com>
2024-10-30 18:29:55 +0000

    Initialized the repo with some basic files

---
+++ app.rb
@@ -0,0 +1,34 @@
+# create basic sinatra app
+require 'sinatra'
+require 'json'
+require 'net/http'
+
+set :port, 3000
+
+post '/webhook' do
+    #get the payload
+    request.body.rewind
+    request.body = request.body.read
+    payload = JSON.parse(request.body)
+    #get event type
+    event_type = request.env['HTTP_X_GITHUB_EVENT']
+
+    # depending on the event type, do something
+    case event_type
+    when 'pull_request'
+        handle_pull_request(payload)
+    when 'issues'
+        handle_issue(payload)
+    else 
+        status 400
+        return "Event type not supported: #{event_type}"
+    end
+
+    status 200
+end
+
+def handle_pull_request(payload)
+end
+
+def handle_issue(payload)
+end
